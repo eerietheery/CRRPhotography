c597312ba3040cc535d7c348a60c534269b1cf02
Trevor York <eerietheery@gmail.com>
2023-05-30 23:07:49 -0700

    midnight oil

--- submit-form.php
+++ submit-form.php
@@ -1,4 +1,8 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
     $email = $_POST['email'];
@@ -18,25 +22,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = htmlspecialchars($email);
     $message = htmlspecialchars($message);
 
-    // Send email using PHPMailer
-    require 'vendor/autoload.php';
-    $mail = new PHPMailer\PHPMailer\PHPMailer();
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = $_ENV['eerietheery@gmail.com'];
-    $mail->Password = $_ENV['PPish00eetsahn!!'];
-    $mail->SMTPSecure = 'tls';
-    $mail->Port = 587;
-    $mail->setFrom($email, $name);
-    $mail->addAddress('eerietheery@gmail.com');
-    $mail->Subject = 'New message from Pet Photography website';
-    $mail->Body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    require 'PHPMailer/Exception.php';
+    require 'PHPMailer/PHPMailer.php';
+    require 'PHPMailer/SMTP.php';
 
-    if ($mail->send()) {
-        echo 'Thank you for your message!';
-    } else {
-        echo 'Something went wrong. Please try again.';
+    // Send email using PHPMailer
+    $mail = new PHPMailer();
+    try {
+        // SMTP Configuration
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'eerietheery@gmail.com'; // Replace with your Gmail Email Address
+        $mail->Password = 'your_gmail_password'; // Replace with your Gmail Password
+        $mail->Port = 587;
+
+        // Email Configuration
+        $mail->setFrom('eerietheery@gmail.com', 'Pet Photography website');
+        $mail->addAddress('recipient@example.com'); // Replace with your desired recipient email address
+        $mail->addReplyTo($email, $name);
+
+        // Email Content
+        $mail->isHTML(true); 
+        $mail->Subject = 'New message from Pet Photography website';
+        $mail->Body = "Name: $name<br>Email: $email<br>Message:<br>$message";
+
+        // Send Email
+        if ($mail->send()) {
+            echo 'Thank you for your message!';
+        } else {
+            echo 'Something went wrong. Please try again.';
+        }
+    } catch (Exception $e) {
+        echo "Mailer Error: {$mail->ErrorInfo}";
     }
 } else {
     header('Location: index.html');
